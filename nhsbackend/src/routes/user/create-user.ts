60ab7fd63161051e4a0b5a65e55a1d5564ebc40c
asifjoardar <mrsparrow04@gmail.com>
2022-06-23 15:36:28 +0600

    user crud feature added

---
+++ nhsbackend/src/routes/user/create-user.ts
@@ -0,0 +1,40 @@
+import {Router} from "express";
+import {UserInfo} from "../entity/user-info";
+const router = Router();
+
+router.post('/user', async (req, res) => {
+    const {
+        name,
+        bloodGroup,
+        gender,
+        dateOfBirth,
+        address,
+        contact,
+        emergencyContactName,
+        emergencyContactNumber,
+        officialIdType,
+        officialIdNumber
+    } = req.body;
+    console.log(req.body)
+
+    const user = UserInfo.create({
+        name: name,
+        bloodGroup: bloodGroup,
+        gender: gender,
+        dob: dateOfBirth,
+        address: address,
+        contact: contact,
+        emergencyContactName: emergencyContactName,
+        emergencyContactNumber: emergencyContactNumber,
+        officialIdType: officialIdType,
+        officialIdTypeNumber: officialIdNumber
+    });
+
+    await user.save();
+
+    return res.json(user);
+});
+
+export {
+    router as createUserRouter
+}
