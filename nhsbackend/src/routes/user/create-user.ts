bdb89e4ed4c67f96cba47a61f13d659deb8d1e7b
asifjoardar <mrsparrow04@gmail.com>
2022-06-23 17:06:24 +0600

    fix create-user issue

--- nhsbackend/src/routes/user/create-user.ts
+++ nhsbackend/src/routes/user/create-user.ts
@@ -3,36 +3,7 @@ import {UserInfo} from "../../entity/user-info";
 const router = Router();
 
 router.post('/api/user', async (req, res) => {
-    const {
-        name,
-        bloodGroup,
-        gender,
-        dateOfBirth,
-        address,
-        contact,
-        emergencyContactName,
-        emergencyContactNumber,
-        officialIdType,
-        officialIdNumber
-    } = req.body;
-    console.log(req.body)
-
-    const user = UserInfo.create({
-        name: name,
-        bloodGroup: bloodGroup,
-        gender: gender,
-        dob: dateOfBirth,
-        address: address,
-        contact: contact,
-        emergencyContactName: emergencyContactName,
-        emergencyContactNumber: emergencyContactNumber,
-        officialIdType: officialIdType,
-        officialIdTypeNumber: officialIdNumber
-    });
-
-    await user.save();
-
-    return res.json(user);
+    return res.json(await UserInfo.create(req.body).save());
 });
 
 export {
