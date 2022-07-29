e3eb14a804c79c5726d43f1782f8e530792bde12
asifjoardar <mrsparrow04@gmail.com>
2022-07-30 03:57:44 +0600

    deploy script updated

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,6 @@
+echo "Kill all the running PM2 actions"
+pm2 kill
+
 echo "Jump to app folder"
 cd /home/ubuntu/health/backend
 
@@ -6,10 +9,10 @@ git pull
 
 echo "Install app dependencies"
 sudo rm -rf node_modules package-lock.json
-sudo npm install
+npm install
 
-echo "Build your app"
-sudo npm run build
+echo "Build app"
+npm run build
 
 echo "Restart PM2 action"
-sudo pm2 restart health-backend
+pm2 restart health-backend
