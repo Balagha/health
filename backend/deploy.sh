6313f459aa1d618537874c741c248bd2d6773a9a
Md. Asif Joardar <mrsparrow04@gmail.com>
2022-07-30 19:50:25 +0600

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,9 +2,11 @@ echo "Jump to app folder"
 cd /home/ubuntu/health/backend
 
 echo "Update app from Git"
+
 git pull
 
 echo "Install app dependencies"
+
 sudo rm -rf node_modules package-lock.json
 npm install
 
