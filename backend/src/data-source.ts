5468f5b0709b5e4b882783e4c08ccd4d4616c377
asifjoardar <mrsparrow04@gmail.com>
2022-08-01 23:26:07 +0600

    connect rds with ec2

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -10,14 +10,22 @@ import {MedicalTestReport} from "./entity/medical-test-report";
 import {DoctorAvailability} from "./entity/doctor-availability";
 import 'dotenv/config'
 export const AppDataSource = new DataSource({
-    type: process.env.TYPE as any,
+    type: "postgres" as any,
+    host: "health-database.cnmhcf4uohzv.us-east-1.rds.amazonaws.com" as any,
+    port: 5432 as any,
+    username: "postgres",
+    password: "postgres",
+    database: "healthdb",
+    synchronize: true as any,
+    logging: true as any,
+    /*type: process.env.TYPE as any,
     host: process.env.HOST as any,
     port: process.env.PORT as any,
     username: process.env.DATABASE_USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     synchronize: process.env.SYNCHRONIZE as any,
-    logging: process.env.LOGGING as any,
+    logging: process.env.LOGGING as any,*/
     entities: [
         User,
         Doctor,
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,20 +5,20 @@ import router from "./routes/routers";
 
 const app =express();
 
+/*
 app.use(BodyParser.json());
 //app.use('/api', router);
 app.get('/api', (req, res) => {
     res.send('Hello World!')
 })
 app.listen(8080, () => console.log("app is listen at port 8080"));
+*/
 
-/*
 AppDataSource.initialize().then(async () => {
     app.use(BodyParser.json());
-    // app.use('/api', router);
-    app.get('/', (req, res) => {
+    app.use('/api', router);
+    /*app.get('/', (req, res) => {
 	  res.send('Hello World!')
-	})
+	})*/
     app.listen(8080, () => console.log("app is listen at port 8080"));
 }).catch(console.log)
-*/
