4da9971ccbf7a8b9ea1127ec140b677027f98405
humayun <humayun200044@diit.edu.bd>
2022-08-05 00:53:48 +0600

    #33 First Section

---
+++ frontend/src/Visitor/Components/Carousel/Slider-controls.tsx
@@ -0,0 +1,19 @@
+function SliderControls(){
+    return(
+        <div>
+            <button type="button" className="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
+                <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+                    <span className="sr-only">Previous</span>
+                </span>
+                    </button>
+            <button type="button" className="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
+                <span className="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+                    <span className="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+    )
+}
+export default SliderControls;
---
+++ frontend/src/Visitor/Components/Carousel/index.tsx
@@ -0,0 +1,18 @@
+import CarouselWrapper from "./Carousel-wrapper";
+import SliderControls from "./Slider-controls";
+import SliderIndicators from "./Slider-indicators";
+
+function Carousel(){
+    return(
+      
+
+<div id="default-carousel" className="relative" data-carousel="static"> 
+    <CarouselWrapper/> 
+    <SliderIndicators/> 
+    <SliderControls/>
+</div>
+
+  
+    );
+};
+export default Carousel;
