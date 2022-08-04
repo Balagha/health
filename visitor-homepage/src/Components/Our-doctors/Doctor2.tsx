5c9b5bef295a7206920804d37637dbdba565590b
Humayun Ahmed <humayun200044@diit.edu.bd>
2022-08-04 07:01:01 +0600

    #33 Image Issue Solve

--- visitor-homepage/src/Components/Our-doctors/Doctor2.tsx
+++ visitor-homepage/src/Components/Our-doctors/Doctor2.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import TeamImage2 from '../../images/team-image2.jpg'
+
 function Doctor1(){
-    return(
-        <div className="px-4">
-                    <div className="rounded-md bg-[#f9f9f9]">
-                        <img src={TeamImage2} alt="" />
-                        <h1 className="pt-5 text-3xl text-left px-6 font-bold">Miasha Nakahara</h1>
-                        <p className="text-[#757575] px-6 pb-6 text-left">General Principal</p>
-                        <hr />
-                        <p className="text-[#757575] text-left pt-6">
-                            <i className="pl-6 pr-3 fa fa-phone"></i>
-                            <span>010-060-0160</span>
-                        </p>
-                        <p className="text-[#757575] text-left pb-6">
-                            <i className="pl-6 pr-3 fa fa-envelope-o"></i>
-                            <span>mail@yourdomain.com</span>
-                        </p>
-                    </div>
-                </div>
-    )
+    return (
+      <div className="px-4">
+        <div className="rounded-md bg-[#f9f9f9]">
+          <img src="/images/team-image2.jpg.jpg" alt="" />
+          <h1 className="pt-5 text-3xl text-left px-6 font-bold">
+            Miasha Nakahara
+          </h1>
+          <p className="text-[#757575] px-6 pb-6 text-left">
+            General Principal
+          </p>
+          <hr />
+          <p className="text-[#757575] text-left pt-6">
+            <i className="pl-6 pr-3 fa fa-phone"></i>
+            <span>010-060-0160</span>
+          </p>
+          <p className="text-[#757575] text-left pb-6">
+            <i className="pl-6 pr-3 fa fa-envelope-o"></i>
+            <span>mail@yourdomain.com</span>
+          </p>
+        </div>
+      </div>
+    );
 }
 export default Doctor1;
