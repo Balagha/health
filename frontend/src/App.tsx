f25eb3ea4161a853d1c1ea1983123e463d9988f9
humayun2000444 <humayun200044@diit.edu.bd>
2022-08-17 00:34:20 +0600

    #67 Table section Added into Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Visitor from "./Visitor/Visitor";
 import DashBoardMainSection from "./dashBoard/Components/dashBoardMain";
+import Table from "./dashBoard/Components/table";
 
 function App() {
   return (
     <div className="font-roboto">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Visitor/>}/>
-                <Route path="/dashboard" element={<DashBoardMainSection/>}/>
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Visitor />} />
+          <Route path="/dashboard/components" element={<Table />} />
+          <Route path="/dashboard" element={<DashBoardMainSection />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
