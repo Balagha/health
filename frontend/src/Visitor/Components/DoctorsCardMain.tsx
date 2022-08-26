be41b39fff275bd0056dedfecacccf7035fb4c1e
NHSanto <nahid170053@diit.edu.bd>
2022-08-27 03:21:59 +0600

    Search and Filter Doctors from Card

--- frontend/src/Visitor/Components/DoctorsCardMain.tsx
+++ frontend/src/Visitor/Components/DoctorsCardMain.tsx
@@ -1,49 +1,94 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from "./Navbar";
 import Topbar from "./Topbar";
 import DoctorsRating from "./DoctorsRating";
+import SearchDoctor from "./SearchDoctor/searchDoctor";
+import {useLocation} from "react-router-dom";
+
 
 function DoctorsCardMain() {
+    const [items, setItems] = useState([])
+    const search = useLocation().search;
+    const query = new URLSearchParams(search).get('search');
+    console.log("get query from doctor card", query);
+    const [isLoading, setLoading] = useState(true);
+    // https://raw.githubusercontent.com/asifjoardar/health/master/misc/dr-list.json #Our Data
+    // http://universities.hipolabs.com/search?country=United+States #Dummmy api
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/asifjoardar/health/master/misc/dr-list.json')
+            .then((res) => res.json())
+            .then((result) => {
+                setItems(result)
+            })
+    }, [])
+
+    const [searchParam] = useState(["name", "title"]);
+
+    function cardSearch(items: any) {
+        return items.filter((item: any) => {
+            return searchParam.some((newItem) => {
+                return (
+                    item[newItem]
+                        .toString()
+                        .toLowerCase()
+                        .includes(query?.toLowerCase())
+                );
+            });
+        });
+    }
+
+    const found = cardSearch(items);
     return (
         <>
             <Topbar/>
             <Navbar/>
-            <div className="flex flex-row flex-wrap mt-10 justify-center">
-                <div className="m-1 h-full w-full max-w-sm bg-white rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex flex-row items-center justify-center">
-                        <div className="flex flex-col">
-                            <img className="p-8 w-80 h-40 rounded-full" src="/images/doctor.png" alt="doctor image"/>
-                        </div>
-                        <div className="flex flex-col">
-                            <h2 className="text-left">MBBS, MFILL, (Microbiology), MCPS, FCPS (Psychiatry)</h2>
-                        </div>
+            <SearchDoctor/>
+            {found.length === 0 ?
+                <>
+                    <div className="flex items-center justify-center ">
+                        <div
+                            className="w-40 h-40 border-t-4 border-b-4 border-green-900 rounded-full animate-spin"></div>
                     </div>
-                    <div className="flex flex-row">
-                        <div className="flex flex-col px-5 pb-5">
-                            <div className="flex flex-row">
-                                <div>
-                                    <a href="src/Visitor/Components/doctorsCardMain#">
-                                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">DR. Munna Bhai</h5>
-                                    </a>
-                                    <DoctorsRating/>
+                </> :
+                <div className="flex flex-row flex-wrap mt-1 mb-2 justify-center">
+                    {found.map((item:any ,index:any) => (
+                        <div key={index}
+                             className="m-1 w-full max-w-sm bg-white rounded-lg shadow-2xl dark:bg-gray-800 dark:border-gray-700">
+                            <div className="flex flex-row items-center justify-center">
+                                <div className="flex flex-col w-[50%]">
+                                    <img className="p-8 w-80 h-40 rounded-full" src="/images/doctor.png"
+                                         alt="doctor image"/>
+                                </div>
+                                <div className="flex flex-col p-2 w-[50%]">
+                                    <h2 className="text-left break-words">{item.title}</h2>
                                 </div>
                             </div>
-                            <div className="flex justify-between items-center">
-                                <a href="src/Visitor/Components/doctorsCardMain#"
-                                   className="text-white bg-[#A5C52C] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Appoinment
-                                </a>
+                            <div className="flex flex-row">
+                                <div className="flex flex-col px-3 pb-2 w-[50%]">
+                                    <div className="flex flex-row">
+                                        <div>
+                                            <a href="src/Visitor/Components/doctorsCardMain#">
+                                                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white break-words">{item["name"]}</h5>
+                                            </a>
+                                            <DoctorsRating/>
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between items-center">
+                                        <a href="src/Visitor/Components/doctorsCardMain#"
+                                           className="text-white bg-[#A5C52C] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get
+                                            Appoinment
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="flex p-2 flex-col w-[50%]">
+                                    <h2 className="text-left">{item.work_place}</h2>
+                                </div>
                             </div>
                         </div>
-                        <div className="flex flex-col">
-                            <h2 className="font-bold">Hospitals</h2>
-                            <h1>Dhaka Medical College</h1>
-                            <h1>Popular Diagnostic</h1>
-                        </div>
-                    </div>
+                    ))}
                 </div>
-            </div>
-
+            }
         </>
-)
+    )
 }
 export default DoctorsCardMain;
--- frontend/src/Visitor/Components/SearchDoctor/searchDoctor.tsx
+++ frontend/src/Visitor/Components/SearchDoctor/searchDoctor.tsx
@@ -1,56 +1,74 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from 'react'
+import {createSearchParams, useNavigate} from 'react-router-dom';
 import DiseaseDropdown from "./diseaseDropdown";
 
-function SearchDoctor() {
-  const navigate = useNavigate();
-  return (
-    <div className="flex flex-col justify-center items-center w-full h-80 rounded-xl">
-      <div className="flex">
-        <h2 className="text-5xl text-[#757575] font-bold mb-10 mt-5">
-          Search Your Doctor
-        </h2>
-      </div>
 
-      <div className="flex justify-center">
-        <DiseaseDropdown />
-        <div className="mb-3 xl:w-96 mt-1">
-          <div className="input-group relative flex items-stretch w-full mb-4">
-            <input
-                onKeyDown={(e) => e.key === 'Enter' &&  navigate("/doctors")}
-              type="search"
-              className="form-control relative flex-auto min-w-0 inline-block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="button-addon2"
-            />
-            <button
-              onClick={()=> navigate("/doctors")}
-              className="btn inline-block px-6 py-2.5 bg-[#a5c422] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#4267b2] hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
-              type="button"
-              id="button-addon2"
-            >
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="search"
-                className="w-4"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-                ></path>
-              </svg>
-            </button>
-          </div>
+function SearchDoctor() {
+    const [query, setQuery] = useState('')
+    const navigate = useNavigate();
+
+    function handleChange(e: any) {
+        setQuery(e.target.value)
+    }
+
+    const handleSubmit = (e: any) => {
+        navigate({
+            pathname: "/doctors",
+            search: createSearchParams({
+                search: query
+            }).toString()
+        });
+        e.preventDefault();
+    };
+
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-40 rounded-xl">
+            <div className="flex">
+                <h2 className="text-5xl text-[#757575] font-bold mb-10 mt-5">
+                  Search Your Doctor
+                </h2>
+            </div>
+
+            <div className="flex justify-center">
+                <DiseaseDropdown/>
+                <div className="mb-3 xl:w-96 mt-1">
+                    <form method="GET" className="input-group relative flex items-stretch w-full mb-4"
+                          onSubmit={handleSubmit}>
+                        <input
+                            onChange={handleChange}
+                            type="search"
+                            className="form-control relative flex-auto min-w-0 inline-block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            placeholder="Search"
+                            aria-label="Search"
+                            aria-describedby="button-addon2"
+                        />
+                        <button
+                            /*  onClick={handleSubmit}*/
+                            className="btn inline-block px-6 py-2.5 bg-[#a5c422] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-[#4267b2] hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+                            type="submit"
+                            id="button-addon2"
+                        >
+                            <svg
+                                aria-hidden="true"
+                                focusable="false"
+                                data-prefix="fas"
+                                data-icon="search"
+                                className="w-4"
+                                role="img"
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path
+                                    fill="currentColor"
+                                    d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+                                ></path>
+                            </svg>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default SearchDoctor;
