d972acb7fea9b9b27b7052c289d637108e1c5333
mdemon420 <mdeamon4@gmail.com>
2022-09-07 20:43:48 +0600

    Tob bar responsive

--- frontend/src/Visitor/Components/Navbar/Desktop-nav.tsx
+++ frontend/src/Visitor/Components/Navbar/Desktop-nav.tsx
@@ -11,7 +11,7 @@ function DesktopNav() {
           <button
             onClick={() => setIsOpen(!isOpen)}
             type="button"
-            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-[#A5C422] focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             aria-controls="mobile-menu"
             aria-expanded="false"
           >
--- frontend/src/Visitor/Components/Topbar/Mobile-section.tsx
+++ frontend/src/Visitor/Components/Topbar/Mobile-section.tsx
@@ -1,42 +1,46 @@
 function MobileSection() {
   return (
-    <div className="sm:hidden text-[#757575]">
-      <div className="px-1 pb-4 space-y-1">
-        <a
-          href="/#"
-          className="text-#393939-300 hover:text-[#a5c422] px-3 py-2 text-sm font-medium"
-        >
-          <img
-            className="w-5 inline-block mr-3"
-            src="/images/phone1.png"
-            alt=""
-          />
-          010-060-0160
-        </a>
-
-        <a
-          href="/#"
-          className="text-#393939-300 hover:text-[#a5c422] pl-10 py-2 text-sm font-medium"
-        >
-          <img
-            className="w-5 inline-block mr-3"
-            src="/images/calender.png"
-            alt=""
-          />
-          6:00 AM - 10:00 PM (Mon-Fri)
-        </a>
-
-        <a
-          href="/#"
-          className="flex justify-center text-#393939-300 hover:text-[#a5c422] px-3 py-2 text-sm font-medium"
-        >
-          <img
-            className="w-5 inline-block mr-3"
-            src="/images/email.png"
-            alt=""
-          />
-          info@company.com
-        </a>
+    <div className="sm:hidden  text-[#757575]">
+      <div className="flex flex-wrap px-1 pb-4 space-y-1">
+        <div>
+          <a
+            href="/#"
+            className="text-#393939-300 hover:text-[#a5c422] px-3 py-2 text-sm font-medium"
+          >
+            <img
+              className="w-5 inline-block mr-3"
+              src="/images/phone1.png"
+              alt=""
+            />
+            010-060-0160
+          </a>
+        </div>
+        <div>
+          <a
+            href="/#"
+            className="text-#393939-300 hover:text-[#a5c422] px-3 py-2 text-sm font-medium"
+          >
+            <img
+              className="w-5 inline-block mr-3"
+              src="/images/calender.png"
+              alt=""
+            />
+            6:00 AM - 10:00 PM (Mon-Fri)
+          </a>
+        </div>
+        <div>
+          <a
+            href="/#"
+            className="text-#393939-300 hover:text-[#a5c422] px-3 py-2 text-sm font-medium"
+          >
+            <img
+              className="w-5 inline-block mr-3"
+              src="/images/email.png"
+              alt=""
+            />
+            info@company.com
+          </a>
+        </div>
       </div>
     </div>
   );
