ed1e6ddaab0e4aada80b89e78f2b492228f9b351
humayun2000444 <humayun200044@diit.edu.bd>
2022-09-23 22:15:55 +0600

    Table section added into dashboard & all conflicts are solved

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Visitor from "./Visitor";
 import Auth from "./Auth";
 import Dashboard from "./Dashboard";
+import Table from "./Dashboard/Components/table";
 
 function App() {
   return (
     <div className="font-roboto">
       <BrowserRouter>
         <Routes>
-          <Route path="/auth/*" element={<Auth/>} />
+          <Route path="/Dashboard/Components/table" element={<Table />} />
+          <Route path="/auth/*" element={<Auth />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/*" element={<Visitor />} />
         </Routes>
